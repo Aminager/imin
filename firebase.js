5af0dec26603ee047e2a69532e0c57b6d7cd9e70
Amin Alian <alianamin00@gmail.com>
2023-11-21 11:44:31 +0100

    Removed config

--- firebase.js
+++ firebase.js
@@ -1,25 +1,17 @@
-// Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 import {getAuth} from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD3vqLiweYYlyo-3D6nBB_Zx6jV9wN9PvM",
-  authDomain: "imin-auth-bd4b0.firebaseapp.com",
-  projectId: "imin-auth-bd4b0",
-  storageBucket: "imin-auth-bd4b0.appspot.com",
-  messagingSenderId: "931226010865",
-  appId: "1:931226010865:web:b3bb5aa461783b7de6d550",
-  measurementId: "G-4RQV78PF76"
-};
+import { FIREBASE_APIKEY, FIREBASE_AUTHDOMAIN, FIREBASE_PROJECTID, FIREBASE_STORAGEBUCKET, FIREBASE_MESSAGINGSENDERID, FIREBASE_APPID, FIREBASE_MEASUREMENTID } from "@env";
 
 // Initialize Firebase
-
-
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp({
+  FIREBASE_APIKEY,
+  FIREBASE_AUTHDOMAIN,
+  FIREBASE_PROJECTID,
+  FIREBASE_STORAGEBUCKET,
+  FIREBASE_MESSAGINGSENDERID,
+  FIREBASE_APPID,
+  FIREBASE_MEASUREMENTID
+});
 export const auth = getAuth();
 export const db = getFirestore(firebaseApp);
